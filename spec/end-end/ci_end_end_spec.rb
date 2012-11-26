084b77b13166911dfb24b5a1ffa46e1a38a45302
Andrew Bruce <andrew@camelpunch.com>
2012-11-26 00:34:25 -0800

    sketched out first end-end test

---
+++ spec/end-end/ci_end_end_spec.rb
@@ -0,0 +1,43 @@
+gem 'minitest'
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'yaml'
+
+describe "CI end-end" do
+  let(:application) { ApplicationRunner.new }
+  let(:repository_url) { 'https://github.com/camelpunch/ci_test_repo.git' }
+  let(:repository) {
+    Repository.new(committer: gmail_config['email'],
+                   url: repository_url)
+  }
+  let(:gmail_config) { YAML.load_file(File.expand_path('../gmail_config.yml', __FILE__)) }
+  let(:mail_client) { GMailClient.new(gmail_config) }
+
+  after do
+    application.stop
+    mail_client.logout
+  end
+
+  it "emails developers about their failed commits (assuming one-commit-per-push)" do
+    application.start
+    application.add_job(job_name: 'unit-tests',
+                        script: 'rake',
+                        repository: repository_url)
+    bad_commit_id = repository.push_bad_commit
+    mail_client.receives_failure_notification_about_commit_id(bad_commit_id)
+  end
+
+  class ApplicationRunner
+    def start
+      # load up the server - rack?
+    end
+
+    def add_job(*)
+      # some capybara stuff
+    end
+  end
+
+  class Repository
+
+  end
+end
