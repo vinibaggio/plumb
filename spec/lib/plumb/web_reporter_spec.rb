87cfa60394a00800bde52c74f39740063f6455ad
Andrew Bruce <andrew@camelpunch.com>
2012-12-23 21:12:11 -0800

    move SpyServer into its own file

--- spec/lib/plumb/web_reporter_spec.rb
+++ spec/lib/plumb/web_reporter_spec.rb
@@ -1,8 +1,5 @@
 require 'minitest/autorun'
-require 'thread'
-require 'webrick'
-require 'tempfile'
-require 'json'
+require_relative '../../support/spy_server'
 require_relative '../../../lib/plumb/web_reporter'
 require_relative '../../../lib/plumb/build_status'
 
@@ -16,43 +13,6 @@ module Plumb
       reporter.build_completed(status)
 
       server.last_request.must_equal ['PUT', status.to_json ]
-    end
-
-    class SpyServer
-      class << self
-        attr_accessor :queue
-
-        def start(port, path)
-          new(port, path).tap(&:start)
-        end
-      end
-
-      def initialize(port, path)
-        @port = port
-        @path = path
-        SpyServer.queue = ::Queue.new
-      end
-
-      def start
-        server.mount(@path, PutServlet)
-        Thread.new { server.start }.abort_on_exception = true
-      end
-
-      def last_request
-        SpyServer.queue.pop(non_block = true)
-      end
-
-      private
-
-      def server
-        @server ||= WEBrick::HTTPServer.new(Port: @port)
-      end
-
-      class PutServlet < WEBrick::HTTPServlet::AbstractServlet
-        def do_PUT(request, _)
-          SpyServer.queue << [request.request_method, request.body]
-        end
-      end
     end
   end
 end
---
+++ spec/support/spy_server.rb
@@ -0,0 +1,42 @@
+require 'thread'
+require 'webrick'
+require 'tempfile'
+require 'json'
+
+class SpyServer
+  class << self
+    attr_accessor :queue
+
+    def start(port, path)
+      new(port, path).tap(&:start)
+    end
+  end
+
+  def initialize(port, path)
+    @port = port
+    @path = path
+    SpyServer.queue = ::Queue.new
+  end
+
+  def start
+    server.mount(@path, PutServlet)
+    Thread.new { server.start }.abort_on_exception = true
+  end
+
+  def last_request
+    SpyServer.queue.pop(non_block = true)
+  end
+
+  private
+
+  def server
+    @server ||= WEBrick::HTTPServer.new(Port: @port)
+  end
+
+  class PutServlet < WEBrick::HTTPServlet::AbstractServlet
+    def do_PUT(request, _)
+      SpyServer.queue << [request.request_method, request.body]
+    end
+  end
+end
+
