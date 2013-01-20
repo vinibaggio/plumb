cee2564f33683f311b95fbafdbd21933a8fc9a4d
Andrew Bruce <andrew@camelpunch.com>
2013-01-20 10:19:14 -0800

    refactor filesystem storage spec

--- spec/lib/plumb/filesystem_job_storage_spec.rb
+++ spec/lib/plumb/filesystem_job_storage_spec.rb
@@ -6,9 +6,9 @@ require_relative '../../../lib/plumb/filesystem_job_storage'
 module Plumb
   describe FileSystemJobStorage do
     it "stores and retrieves jobs, across instances" do
-      Dir.mktmpdir do |dir|
-        storage1 = FileSystemJobStorage.new('test', Pathname(dir).join('db.json'))
-        storage2 = FileSystemJobStorage.new('test', Pathname(dir).join('db.json'))
+      with_nonexistent_file_path do |path|
+        storage1 = FileSystemJobStorage.new('test', path)
+        storage2 = FileSystemJobStorage.new('test', path)
 
         storage1 << job = Job.new(foo: 'bar')
         storage1.jobs.must_equal [job]
@@ -17,15 +17,21 @@ module Plumb
     end
 
     it "clears all jobs, across instances" do
-      Dir.mktmpdir do |dir|
-        storage1 = FileSystemJobStorage.new('test', Pathname(dir).join('db.json'))
-        storage2 = FileSystemJobStorage.new('test', Pathname(dir).join('db.json'))
+      with_nonexistent_file_path do |path|
+        storage1 = FileSystemJobStorage.new('test', path)
+        storage2 = FileSystemJobStorage.new('test', path)
         storage1 << job = Job.new(foo: 'bar')
 
         storage2.jobs.wont_be :empty?
         storage1.clear
         storage1.jobs.must_be :empty?
         storage2.jobs.must_be :empty?
+      end
+    end
+
+    def with_nonexistent_file_path
+      Dir.mktmpdir do |dir|
+        yield Pathname(dir).join('db.json')
       end
     end
   end
